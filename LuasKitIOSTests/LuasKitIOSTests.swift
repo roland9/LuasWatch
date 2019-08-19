b00506206501b7ba46618762862dbf89adadb8d0
Roland Gropmair <roland@mapps.ie>
2019-08-19 22:28:44 +0100

    add some tests

--- LuasKitIOSTests/LuasKitIOSTests.swift
+++ LuasKitIOSTests/LuasKitIOSTests.swift
@@ -10,33 +10,88 @@ import LuasKitIOS
 
 class LuasKitIOSTests: XCTestCase {
 
-	override func setUp() {
-		// Put setup code here. This method is called before the invocation of each test method in the class.
-	}
+	let train1 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "Due")
+	let train2 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "9")
+	let train3 = Train(destination: "LUAS Sandyford", direction: "Inbound", dueTime: "12")
 
-	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-	}
+	let station = TrainStation(stationId: "stationId",
+							   stationIdShort: "LUAS8",
+							   name: "Bluebell",
+							   location: CLLocation(latitude: CLLocationDegrees(Double(1.1)), longitude: CLLocationDegrees(Double(1.2))))
 
-	func testExample() {
-		let location = CLLocation(latitude: CLLocationDegrees(Double(1.1)), longitude: CLLocationDegrees(Double(1.2)))
-
-		let train1 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "Due")
-		let train2 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "9")
-		let train3 = Train(destination: "LUAS Sandyford", direction: "Inbound", dueTime: "12")
-
-		let station = TrainStation(stationId: "stationId",
-								   stationIdShort: "LUAS8",
-								   name: "Bluebell",
-								   location: location)
+	func testDueTimeDescription() {
 
 		let trains = TrainsByDirection(trainStation: station,
 									   inbound: [train3],
 									   outbound: [train1, train2])
 
 		XCTAssert(trains.inbound.count == 1)
-		XCTAssert(trains.outbound.count == 2)
+		XCTAssert(trains.inbound[0].dueTimeDescription == "Sandyford: 12 mins")
 
+		XCTAssert(trains.outbound.count == 2)
+		XCTAssert(trains.outbound[0].dueTimeDescription == "Broombridge: Due")
+		XCTAssert(trains.outbound[1].dueTimeDescription == "Broombridge: 9 mins")
+	}
+
+	func testRealAPI() {
+		let apiExpectation = expectation(description: "API call expectation")
+
+		LuasAPI.dueTime(for: station) { (result) in
+			switch result {
+
+			case .error(let message):
+				print("error: \(message)")
+
+			case .success(let trains):
+				apiExpectation.fulfill()
+				print(trains)
+			}
+		}
+
+		wait(for: [apiExpectation], timeout: 10)
+	}
+
+	func testMockAPI() {
+		let apiExpectation = expectation(description: "API call expectation")
+
+		/*
+			[
+				"destination": "LUAS Bride's Glen",
+				"direction": "Outbound",
+				"duetime": "Due"
+			],
+			[
+				"destination": "LUAS Broombridge",
+				"direction": "Inbound",
+				"duetime": "6"
+			],
+			[
+				"destination": "LUAS Bride's Glen",
+				"direction": "Outbound",
+				"duetime": "15"
+			]
+		*/
+
+		LuasMockAPI.dueTime(for: station) { (result) in
+			switch result {
+
+			case .error(let message):
+				print("error: \(message)")
+
+			case .success(let trains):
+				XCTAssert(trains.inbound.count == 1)
+				XCTAssert(trains.inbound[0] == Train(destination: "LUAS Broombridge", direction: "Inbound", dueTime: "6"))
+
+				XCTAssert(trains.outbound.count == 2)
+				XCTAssert(trains.outbound[0] == Train(destination: "LUAS Bride's Glen", direction: "Outbound", dueTime: "Due"))
+				XCTAssert(trains.outbound[1] == Train(destination: "LUAS Bride's Glen", direction: "Outbound", dueTime: "15"))
+
+				apiExpectation.fulfill()
+				print(trains)
+			}
+		}
+
+		wait(for: [apiExpectation], timeout: 1)
 	}
 
 }
