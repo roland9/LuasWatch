4a1085110da24eb0be606abcdb82f7ae4978d91d
Roland Gropmair <roland@mapps.ie>
2019-08-24 12:19:18 +0100

    refactor contentView (introduce groups); add updating states

--- LuasWatch WatchKit Extension/ContentView.swift
+++ LuasWatch WatchKit Extension/ContentView.swift
@@ -8,6 +8,29 @@ import Combine
 import CoreLocation
 import LuasKit
 
+struct TrainsList: View {
+	let trains: TrainsByDirection
+
+	var body: some View {
+
+		List {
+			Section(header: Text("Inbound")) {
+
+				ForEach(trains.inbound, id: \.id) {
+					Text($0.dueTimeDescription)
+				}
+			}
+
+			Section(header: Text("Outbound")) {
+
+				ForEach(trains.outbound, id: \.id) {
+					Text($0.dueTimeDescription)
+				}
+			}
+		}
+	}
+}
+
 struct ContentView: View {
 	@EnvironmentObject var appState: AppState
 
@@ -51,89 +74,94 @@ struct ContentView: View {
 					.padding(.horizontal)
 			)
 
-		case .foundDueTimes(let trainsByDirection):
+		case .foundDueTimes(let trains):
 			return AnyView (
-
 				VStack {
 
-					Text(trainsByDirection.trainStation.name)
+					Text(trains.trainStation.name)
 						.font(.system(.headline))
 
-					List {
-						Section(header: Text("Inbound")) {
-
-							ForEach(trainsByDirection.inbound, id: \.id) {
-								Text($0.dueTimeDescription)
-							}
-						}
-
-						Section(header: Text("Outbound")) {
-
-							ForEach(trainsByDirection.outbound, id: \.id) {
-								Text($0.dueTimeDescription)
-							}
-						}
-					}
-
+					TrainsList(trains: trains)
 				}
 			)
+
+		case .updatingLocation(let trains):
+			return AnyView (
+				VStack {
+
+					Text(trains.trainStation.name)
+						.font(.system(.headline))
+
+					Text("Updating location")
+						.font(.system(.footnote))
+
+					TrainsList(trains: trains)
+				}
+			)
+
+		case .updatingDueTimes(let trains):
+			return AnyView (
+				VStack {
+
+					Text(trains.trainStation.name)
+						.font(.system(.headline))
+
+					Text("Updating due times")
+						.font(.system(.footnote))
+
+					TrainsList(trains: trains)
+				}
+			)
+
 		}
 	}
 }
 
 #if DEBUG
-let appState = AppState()
 let location = CLLocation(latitude: CLLocationDegrees(Double(1.1)),
 						  longitude: CLLocationDegrees(Double(1.2)))
 
+let train1 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "Due")
+let train2 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "9")
+let train3 = Train(destination: "LUAS Sandyford", direction: "Inbound", dueTime: "12")
+
+let station = TrainStation(stationId: "stationId",
+						   stationIdShort: "LUAS8",
+						   name: "Bluebell",
+						   location: location)
+
+let trains = TrainsByDirection(trainStation: station,
+							   inbound: [train3],
+							   outbound: [train1, train2])
+
 // swiftlint:disable:next type_name
-struct ContentView_Preview_GettingLocation: PreviewProvider {
+struct Preview_AppStartup: PreviewProvider {
 	static var previews: some View {
 
-		appState.state = .gettingLocation
+		Group {
+			ContentView().environmentObject(AppState(state: .gettingLocation)).previewDisplayName("getting location")
 
-		return ContentView().environmentObject(appState)
+			ContentView().environmentObject(AppState(state: .gettingStation(location))).previewDisplayName("getting station")
+
+			ContentView().environmentObject(AppState(state:
+				.gettingDueTimes(TrainStation(stationId: "stationId",
+											  stationIdShort: "LUAS70",
+											  name: "Cabra",
+											  location: location)))).previewDisplayName("getting due times")
+		}
 	}
 }
 
-struct ContentView_Preview_GettingStation: PreviewProvider {
+struct Preview_AppRunning: PreviewProvider {
 	static var previews: some View {
 
-		appState.state = .gettingStation(location)
+		Group {
+			ContentView().environmentObject(AppState(state: .foundDueTimes(trains))).previewDisplayName("found first due times")
 
-		return ContentView().environmentObject(appState)
-	}
-}
+			ContentView().environmentObject(AppState(state: .updatingLocation(trains))).previewDisplayName("updating location")
 
-struct ContentView_Preview_GettingDueTimes: PreviewProvider {
-	static var previews: some View {
-
-		let trainStation = TrainStation(stationId: "stationId", stationIdShort: "LUAS70", name: "Cabra", location: location)
-
-		appState.state = .gettingDueTimes(trainStation)
-
-		return ContentView().environmentObject(appState)
-	}
-}
-
-struct ContentView_Preview_Done: PreviewProvider {
-	static var previews: some View {
-
-		let train1 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "Due")
-		let train2 = Train(destination: "LUAS Broombridge", direction: "Outbound", dueTime: "9")
-		let train3 = Train(destination: "LUAS Sandyford", direction: "Inbound", dueTime: "12")
-
-		let station = TrainStation(stationId: "stationId",
-								   stationIdShort: "LUAS8",
-								   name: "Bluebell",
-								   location: location)
-
-		let trains = TrainsByDirection(trainStation: station,
-									   inbound: [train3],
-									   outbound: [train1, train2])
-		appState.state = .foundDueTimes(trains)
-
-		return ContentView().environmentObject(appState)
+			ContentView().environmentObject(AppState(state: .updatingDueTimes(trains))).previewDisplayName("updating due times")
+		}
 	}
 }
 #endif
