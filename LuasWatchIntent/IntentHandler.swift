0c162cc890f9d56c7d8bae6562b9cd12e09f2591
Roland Gropmair <roland@mapps.ie>
2019-12-23 21:24:31 +0000

    adding intenthandler (empty)

---
+++ LuasWatchIntent/IntentHandler.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Roland Gropmair on 23/12/2019.
+//  Copyright © 2019 mApps.ie. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+
+	override func handler(for intent: INIntent) -> Any {
+		// This is the default implementation.  If you want different objects to handle different intents,
+		// you can override this and return the handler you want for that particular intent.
+
+		return self
+	}
+
+}
