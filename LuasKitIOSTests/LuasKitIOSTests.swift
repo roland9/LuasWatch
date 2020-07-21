16bd48604d4b9dd27dd31ad20f45b0f76ca24741
Roland Gropmair <roland@mapps.ie>
2020-07-21 23:20:22 +0100

    skip testing real API; use XR

--- LuasKitIOSTests/LuasKitIOSTests.swift
+++ LuasKitIOSTests/LuasKitIOSTests.swift
@@ -145,19 +145,19 @@ class LuasKitIOSTests: XCTestCase {
 		let view = ContentView()
 			.environmentObject(AppState(state: .errorGettingStation(LuasStrings.tooFarAway)))
 		assertSnapshot(matching: view, as: .image(layout: .device(config: .iPhoneSe),
-												  traits: .init(userInterfaceStyle: .light)), named: "iPhoneSe tooFarAway")
+												  traits: .init(userInterfaceStyle: .light)), named: "iPhoneXR tooFarAway")
 
 		let viewTrains = ContentView()
 			.environmentObject(AppState(state: .foundDueTimes(trainsRed_2_1)))
 		assertSnapshot(matching: viewTrains, as: .image(layout: .device(config: .iPhoneSe),
-														traits: .init(userInterfaceStyle: .light)), named: "iPhoneSe trains")
+														traits: .init(userInterfaceStyle: .light)), named: "iPhoneXR trains")
 
 		let viewError = ContentView()
 			.environmentObject(
 				AppState(state: .errorGettingDueTimes(String(format: LuasStrings.emptyDueTimesErrorMessage, "Cabra"))))
 			.environment(\.sizeCategory, .extraExtraLarge)
 		assertSnapshot(matching: viewError, as: .image(layout: .device(config: .iPhoneSe),
-													   traits: .init(userInterfaceStyle: .light)), named: "iPhoneSe errorEmpty")
+													   traits: .init(userInterfaceStyle: .light)), named: "iPhoneXR errorEmpty")
 
 	}
 }
