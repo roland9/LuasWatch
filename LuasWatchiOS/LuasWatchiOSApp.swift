45217620659984a48bd7c3ba7172df1df966eead
Roland Gropmair <roland@mapps.ie>
2023-04-09 15:48:48 +0100

    adding new iOS target to house the App Intent

---
+++ LuasWatchiOS/LuasWatchiOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  LuasWatchiOSApp.swift
+//  LuasWatchiOS
+//
+//  Created by Roland Gropmair on 09/04/2023.
+//  Copyright © 2023 mApps.ie. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LuasWatchiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
