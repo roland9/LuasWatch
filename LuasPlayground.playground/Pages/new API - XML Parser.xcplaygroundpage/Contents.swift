90b661d2bded522d0f62c3602872afc91a60fd5b
Roland Gropmair <roland@mapps.ie>
2023-10-21 15:33:00 +0100

    fix playgrounds

--- LuasPlayground.playground/Pages/new API - XML Parser.xcplaygroundpage/Contents.swift
+++ LuasPlayground.playground/Pages/new API - XML Parser.xcplaygroundpage/Contents.swift
@@ -6,21 +6,22 @@ import CoreLocation
  <message>Green Line services operating normally</message>
  <direction name="Inbound">
  <tram dueMins="Due" destination="Broombridge" />
- 	</direction>
+     </direction>
  <direction name="Inbound">
  <tram dueMins="5" destination="Broombridge" />
- 	</direction>
+     </direction>
  <direction name="Outbound">
  <tram dueMins="7" destination="Bride's Glen" />
- 	</direction>
+     </direction>
  <direction name="Outbound">
  <tram dueMins="9" destination="Bride's Glen" />
- 	</direction>
+     </direction>
  <direction name="Outbound">
  <tram dueMins="15" destination="Bride's Glen" />
- 	</direction>
+     </direction>
  </stopInfo>
 */
+import LuasKitIOS
 
 /* another example input
  <stopInfo created="2020-09-10T20:35:43" stop="St. Stephen's Green" stopAbv="STS">
@@ -59,146 +60,146 @@ let data = (xml as NSString).data(using: String.Encoding.utf8.rawValue)!
 // credit to https://github.com/nsscreencast/425-parsing-xml-in-swift/blob/master/SwiftParsingXML/SwiftParsingXML/main.swift
 
 protocol ParserDelegate: XMLParserDelegate {
-	var delegateStack: ParserDelegateStack? { get set }
-	func didBecomeActive()
+    var delegateStack: ParserDelegateStack? { get set }
+    func didBecomeActive()
 }
 
 extension ParserDelegate {
-	func didBecomeActive() { }
+    func didBecomeActive() { }
 }
 
 protocol NodeParser: ParserDelegate {
-	associatedtype Item
-	var result: Item? { get }
+    associatedtype Item
+    var result: Item? { get }
 }
 
 class ParserDelegateStack {
-	private var parsers: [ParserDelegate] = []
-	private let xmlParser: XMLParser
+    private var parsers: [ParserDelegate] = []
+    private let xmlParser: XMLParser
 
-	init(xmlParser: XMLParser) {
-		self.xmlParser = xmlParser
-	}
+    init(xmlParser: XMLParser) {
+        self.xmlParser = xmlParser
+    }
 
-	func push(_ parser: ParserDelegate) {
-		parser.delegateStack = self
-		xmlParser.delegate = parser
-		parsers.append(parser)
-	}
+    func push(_ parser: ParserDelegate) {
+        parser.delegateStack = self
+        xmlParser.delegate = parser
+        parsers.append(parser)
+    }
 
-	func pop() {
-		parsers.removeLast()
-		if let next = parsers.last {
-			xmlParser.delegate = next
-			next.didBecomeActive()
-		} else {
-			xmlParser.delegate = nil
-		}
-	}
+    func pop() {
+        parsers.removeLast()
+        if let next = parsers.last {
+            xmlParser.delegate = next
+            next.didBecomeActive()
+        } else {
+            xmlParser.delegate = nil
+        }
+    }
 }
 
 class MessageParser: NSObject, NodeParser {
-	var delegateStack: ParserDelegateStack?
-	var result: String?
+    var delegateStack: ParserDelegateStack?
+    var result: String?
 
-	private var message: String?
+    private var message: String?
 
-	override init() { }
+    override init() { }
 
-	func parser(_ parser: XMLParser, foundCharacters string: String) {
-		message = string
-	}
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        message = string
+    }
 
-	func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-		print("📄 \(self.classForCoder) message: \(String( describing: message))")
-		result = message
-		delegateStack?.pop()
-	}
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        print("📄 \(self.classForCoder) message: \(String( describing: message))")
+        result = message
+        delegateStack?.pop()
+    }
 }
 
 class DirectionParser: NSObject, NodeParser {
-	var result: [Train]? = []
-	var delegateStack: ParserDelegateStack?
+    var result: [Train]? = []
+    var delegateStack: ParserDelegateStack?
 
-	var direction: String
+    var direction: String
 
-	init(direction: String) {
-		self.direction = direction
-	}
+    init(direction: String) {
+        self.direction = direction
+    }
 
-	func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
-				attributes attributeDict: [String: String] = [:]) {
-		print("🔛 \(self.classForCoder) parsing \(elementName) attributeDict \(attributeDict)")
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?,
+                attributes attributeDict: [String: String] = [:]) {
+        print("🔛 \(self.classForCoder) parsing \(elementName) attributeDict \(attributeDict)")
 
-		if elementName == "tram" {
-			if // let destination = attributeDict["destination"],
-			   let dueMins = attributeDict["dueMins"] {
-				if description != "No trams forecast" && dueMins != "" {
-					let train = Train(destination: attributeDict["destination"]!, direction: direction, dueTime: attributeDict["dueMins"]!)
-					result?.append(train)
-				}
-			}
-		}
-	}
+        if elementName == "tram" {
+            if // let destination = attributeDict["destination"],
+               let dueMins = attributeDict["dueMins"] {
+                if description != "No trams forecast" && dueMins != "" {
+                    let train = Train(destination: attributeDict["destination"]!, direction: direction, dueTime: attributeDict["dueMins"]!)
+                    result?.append(train)
+                }
+            }
+        }
+    }
 
-	func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-		if elementName == "direction" {
-			delegateStack?.pop()
-		}
-	}
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "direction" {
+            delegateStack?.pop()
+        }
+    }
 
 }
 
 class StopInfoParser: NSObject, NodeParser {
-	var delegateStack: ParserDelegateStack?
-	var result: TrainsByDirection?
+    var delegateStack: ParserDelegateStack?
+    var result: TrainsByDirection?
 
-	private let trainStation: TrainStation
+    private let trainStation: TrainStation
 
-	private let messageParser = MessageParser()
-	private let directionsInboundParser = DirectionParser(direction: "Inbound")
-	private let directionsOutboundParser = DirectionParser(direction: "Outbound")
+    private let messageParser = MessageParser()
+    private let directionsInboundParser = DirectionParser(direction: "Inbound")
+    private let directionsOutboundParser = DirectionParser(direction: "Outbound")
 
-	init(trainStation: TrainStation) {
-		self.trainStation = trainStation
-	}
+    init(trainStation: TrainStation) {
+        self.trainStation = trainStation
+    }
 
-	func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?,
-				qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
-		print("\(self.classForCoder) parsing \(elementName) attributeDict \(attributeDict)")
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?,
+                qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
+        print("\(self.classForCoder) parsing \(elementName) attributeDict \(attributeDict)")
 
-		switch elementName {
-			case "stopInfo":
-				// we don't need to parse that info; we hand that in based on the API call we're making for the station
-				print("skip stopInfo")
+        switch elementName {
+            case "stopInfo":
+                // we don't need to parse that info; we hand that in based on the API call we're making for the station
+                print("skip stopInfo")
 
-			case "message":
-				delegateStack?.push(messageParser)
+            case "message":
+                delegateStack?.push(messageParser)
 
-			case "direction":
+            case "direction":
 
-				if attributeDict["name"] == "Inbound" {
-					delegateStack?.push(directionsInboundParser)
+                if attributeDict["name"] == "Inbound" {
+                    delegateStack?.push(directionsInboundParser)
 
-				} else if attributeDict["name"] == "Outbound" {
-					delegateStack?.push(directionsOutboundParser)
-			}
+                } else if attributeDict["name"] == "Outbound" {
+                    delegateStack?.push(directionsOutboundParser)
+            }
 
-			default: break
-		}
-	}
+            default: break
+        }
+    }
 
-	func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
 
-		let message: String? = messageParser.result
+        let message: String? = messageParser.result
 
-		result =
-			TrainsByDirection(trainStation: trainStation,
-							  inbound: directionsInboundParser.result ?? [],
-							  outbound: directionsOutboundParser.result ?? [],
-							  message: message)
-		delegateStack?.pop()
-	}
+        result =
+            TrainsByDirection(trainStation: trainStation,
+                              inbound: directionsInboundParser.result ?? [],
+                              outbound: directionsOutboundParser.result ?? [],
+                              message: message)
+        delegateStack?.pop()
+    }
 }
 
 let xmlData = xml.data(using: .utf8)!
@@ -207,15 +208,15 @@ let delegateStack = ParserDelegateStack(xmlParser: xmlParser)
 
 // we know this information based on the user location, no need to parse the xml for that
 let trainStation = TrainStation(stationId: "theStationId", stationIdShort: "stationIdShort", shortCode: "RAN",
-								route: .green, name: "Ranelagh", location: CLLocation(), stationType: .twoway)
+                                route: .green, name: "Ranelagh", location: CLLocation(), stationType: .twoway)
 
 let stopInfoParser = StopInfoParser(trainStation: trainStation)
 delegateStack.push(stopInfoParser)
 
 if xmlParser.parse() {
-	print("Done parsing")
-	print(stopInfoParser.result!)
+    print("Done parsing")
+    print(stopInfoParser.result!)
 } else {
-	print("Invalid xml", xmlParser.parserError?.localizedDescription ?? "")
+    print("Invalid xml", xmlParser.parserError?.localizedDescription ?? "")
 }
 
--- LuasPlayground.playground/Pages/new API .xcplaygroundpage/Contents.swift
+++ LuasPlayground.playground/Pages/new API .xcplaygroundpage/Contents.swift
@@ -1,42 +1,18 @@
 import CoreLocation
 import PlaygroundSupport
+import LuasKitIOS
 
 let userLocation = CLLocation(latitude: CLLocationDegrees(53.3163934083453), longitude: CLLocationDegrees(-6.25344151996991))
 
-// swiftlint:disable line_length
+// swiftlint:disable:next line_length
 // list from https://github.com/mcevoyki2/finalproject/blob/bf95904206107d45743c83bf7bc951c5c99f26b6/move-app/src/app/%2Bluas/enum/luas-stops.enum.ts
 let station = TrainStation(stationId: "stationId",
-						   stationIdShort: "LUAS28",
-						   shortCode: "BEE",
-						   route: .green,
-						   name: "Beechwood",
-						   location: userLocation)
+                           stationIdShort: "LUAS28",
+                           shortCode: "BEE",
+                           route: .green,
+                           name: "Beechwood",
+                           location: userLocation)
 
-LuasAPI2.dueTime(for: station) { (result) in
-	switch result {
-		case .error(let message):
-			print(message)
-		case .success(let trains):
-			print(trains)
-	}
-}
+let realAPI = LuasAPI(apiWorker: RealAPIWorker())
 
-LuasMockAPI2.dueTime(for: station) { (result) in
-	switch result {
-	case .error(let message):
-		print(message)
-	case .success(let trains):
-		print(trains)
-	}
-}
-
-LuasMockEmptyAPI2.dueTime(for: station) { (result) in
-	switch result {
-		case .error(let message):
-			print(message)
-		case .success(let trains):
-			print(trains)
-	}
-}
-
-PlaygroundPage.current.needsIndefiniteExecution = true
+print(try await realAPI.dueTimes(for: station))
