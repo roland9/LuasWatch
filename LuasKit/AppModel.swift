9c04c39bfb5790257a42fbad028f5f92eeb77016
Roland Gropmair <roland@mapps.ie>
2024-01-14 22:48:42 +0000

    adding appMode storage with manual Codable

--- LuasKit/AppModel.swift
+++ LuasKit/AppModel.swift
@@ -13,7 +13,6 @@ import SwiftUI
 public class AppModel: ObservableObject {
 
     /// state machine, drives UI
-    #warning("Codeable does not work here, but it should be supported since Swift 5.5??")
     public enum AppState {
 
         case idle
@@ -37,7 +36,7 @@ public class AppModel: ObservableObject {
     }
 
     /// how user decided how current station should be determined
-    public enum AppMode {
+    public enum AppMode: Equatable {
 
         /// need location
         case closest
@@ -66,13 +65,14 @@ public class AppModel: ObservableObject {
 
     @Published public var appState: AppState = .idle
 
-//    @AppStorage("simpleAppMode")
-//    var simpleAppMode: Int?
-
-
     @Published public var appMode: AppMode = .closest {
         didSet {
-//            simpleAppMode = appMode.simple
+            do {
+                let encoded = try JSONEncoder().encode(appMode)
+                UserDefaults.standard.set(encoded, forKey: "AppMode")
+            } catch {
+                myPrint("error encoding appMode \(error)")
+            }
         }
     }
 
@@ -88,15 +88,20 @@ public class AppModel: ObservableObject {
     @Published public var latestLocation: CLLocation?
 
     public init() {
-        self.appMode = .closest //  AppMode.from(simpleAppMode)
+        if let storedAppModeData = UserDefaults.standard.object(forKey: "AppMode") as? Data,
+           let storedAppMode = try? JSONDecoder().decode(AppMode.self, from: storedAppModeData) {
+            self.appMode = storedAppMode
+        } else {
+            self.appMode = .closest
+        }
+
+        self.selectedStation = TrainStations.sharedFromFile.station(shortCode: "HAR")
     }
 
     // for previews
     public init(_ state: AppState) {
         self.appState = state
     }
-
-    //public var changeable: AppStateChangeable?
 
     // that should be somewhere else
     public func updateWithAnimation(to state: AppState) {
@@ -108,7 +113,88 @@ public class AppModel: ObservableObject {
     }
 }
 
-extension AppModel.AppMode: Equatable {}
+extension AppModel.AppMode: Codable {
+
+    // https://stackoverflow.com/questions/69979095/codable-enum-with-arguments-and-fails-at-compile-time
+
+    private enum CodingBase: String, Codable {
+        case closest
+        case closestOtherLine
+        case favourite // (TrainStation)
+        case nearby // (TrainStation)
+        case specific // (TrainStation)
+        case recents // (TrainStation)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case base
+        case stationValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let base = try container.decode(CodingBase.self, forKey: .base)
+
+        switch base {
+
+            case .closest:
+                self = .closest
+            case .closestOtherLine:
+                self = .closestOtherLine
+            case .favourite:
+                let shortCode = try container.decode(String.self, forKey: .stationValue)
+                if let station = TrainStations.sharedFromFile.station(shortCode: shortCode) {
+                    self = .favourite(station)
+                } else {
+                    self = .closest
+                }
+            case .nearby:
+                let shortCode = try container.decode(String.self, forKey: .stationValue)
+                if let station = TrainStations.sharedFromFile.station(shortCode: shortCode) {
+                    self = .nearby(station)
+                } else {
+                    self = .closest
+                }
+            case .specific:
+                let shortCode = try container.decode(String.self, forKey: .stationValue)
+                if let station = TrainStations.sharedFromFile.station(shortCode: shortCode) {
+                    self = .specific(station)
+                } else {
+                    self = .closest
+                }
+            case .recents:
+                let shortCode = try container.decode(String.self, forKey: .stationValue)
+                if let station = TrainStations.sharedFromFile.station(shortCode: shortCode) {
+                    self = .recents(station)
+                } else {
+                    self = .closest
+                }
+
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+            case .closest:
+                try container.encode(CodingBase.closest, forKey: .base)
+            case .closestOtherLine:
+                try container.encode(CodingBase.closestOtherLine, forKey: .base)
+            case .favourite(let station):
+                try container.encode(CodingBase.favourite, forKey: .base)
+                try container.encode(station.shortCode, forKey: .stationValue)
+            case .nearby(let station):
+                try container.encode(CodingBase.nearby, forKey: .base)
+                try container.encode(station.shortCode, forKey: .stationValue)
+            case .specific(let station):
+                try container.encode(CodingBase.specific, forKey: .base)
+                try container.encode(station.shortCode, forKey: .stationValue)
+            case .recents(let station):
+                try container.encode(CodingBase.recents, forKey: .base)
+                try container.encode(station.shortCode, forKey: .stationValue)
+        }
+    }
+}
 
 extension AppModel.AppState: CustomStringConvertible {
 
