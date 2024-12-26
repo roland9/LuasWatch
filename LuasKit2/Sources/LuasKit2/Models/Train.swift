92bb15eb3938398bca91cb0ba0cec8f5a5111deb
Roland Gropmair <roland@mapps.ie>
2024-12-26 20:16:08 +0000

    adding more models
    * WIP tests

---
+++ LuasKit2/Sources/LuasKit2/Models/Train.swift
@@ -0,0 +1,50 @@
+//
+//  Created by Roland Gropmair on 23/04/2023.
+//  Copyright © 2023 mApps.ie. All rights reserved.
+//
+
+import Foundation
+
+public struct Train: CustomStringConvertible, Hashable, Codable {
+
+  // MARK: - Properties
+  
+  public let destination: String
+  public let direction: String
+  public let dueTime: String
+
+  public var id: String {
+    UUID().uuidString
+  }
+
+  // MARK: - Initializer
+
+  public init(destination: String, direction: String, dueTime: String) {
+    self.destination = destination
+    self.direction = direction
+    self.dueTime = dueTime
+  }
+
+  // MARK: - Computed Properties
+
+  public var description: String {
+    "\(destination.replacingOccurrences(of: "LUAS ", with: "")): \'\(dueTimeDescription)\'"
+  }
+
+  public var dueTimeDescription: String {
+    "\(destination.replacingOccurrences(of: "LUAS ", with: "")): "
+      + ((dueTime.lowercased() == "due") ? "Due" : "\(dueTime) mins")
+  }
+
+//  public var dueTimeDescription2: String {
+//    (dueTime.lowercased() == "due") ? "Due" : dueTime
+//  }
+
+  public var destinationDescription: String {
+    destination.replacingOccurrences(of: "LUAS ", with: "")
+  }
+
+  public var destinationDueTimeDescription: String {
+    "Luas to \(destination) \(dueTime.lowercased() == "due" ? "is Due" : "in \(dueTime)")"
+  }
+}
